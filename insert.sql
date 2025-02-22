3bb01038d93bef6895ff75572c03066a34f4517d
maryxp <117952348+maryxp@users.noreply.github.com>
2025-02-21 20:29:00 -0400

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,77 +1,41 @@
  
 -- Insert
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (25567845,'Luisa','Carmona','lc@gmail.com','F','1996/05/03','2024/08/03');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (5579825,'Pedro','Perez','pp@gmail.com','M','1962/06/02','2023/10/09');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (5487769,'Jose','Paez','jp@gmail.com','M','1962/05/03','2020/07/02');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (12456782,'Josefa','Guitierrez','jg@gmail.com','F','1970/04/10','2024/03/01');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (10213576,'Emma','Libardo','emma245@gmail.com','F','1968/05/03','2024/08/03');
 
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (20413679,'Alma','Jarabillo','almaj578@gmail.com','F','1990/11/01','2021/08/03');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (7546821,'Carlos','Garcia','cg878@gmail.com','M','1965/08/23','2022/09/28');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (25646991,'Estuart','Rodriguez','er@gmail.com','M','1996/03/12','2020/10/03');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (12333465,'Eduardo','Gutierrez','eg202@gmail.com','M','1972/03/31','2019/07/15');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (17458946,'Carmen','Diaz','cd314@gmail.com','F','1985/05/24','2024/08/03');
+INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro) VALUES
+(25567845,'Luisa','Carmona','lc@gmail.com','F','1996/05/03','2024/08/03'),
+(5579825,'Pedro','Perez','pp@gmail.com','M','1962/06/02','2023/10/09'),
+(5487769,'Jose','Paez','jp@gmail.com','M','1962/05/03','2020/07/02'),
+(12456782,'Josefa','Guitierrez','jg@gmail.com','F','1970/04/10','2024/03/01'),
+(10213576,'Emma','Libardo','emma245@gmail.com','F','1968/05/03','2024/08/03'),
 
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (3965481,'Luis','Fernandez','lf@hotmail.com','M','1950/04/18','2016/04/26');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (25221031,'Miguel','Fernandes','mf@hotmail.com','M','1995/08/12','2024/09/13');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (19876324,'Jose','Lopez','jl@hotmail.com','M','1980/04/22','2023/09/07');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (26546128,'Paul','Moreno','paul1.0@hotmail.com','M','1998/06/14','2025/10/01');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (30111714,'Miranda','Perez','mperez@hotmail.com','F','2003/05/04','2023/11/21');
+(20413679,'Alma','Jarabillo','almaj578@gmail.com','F','1990/11/01','2021/08/03'),
+(7546821,'Carlos','Garcia','cg878@gmail.com','M','1965/08/23','2022/09/28'),
+(25646991,'Estuart','Rodriguez','er@gmail.com','M','1996/03/12','2020/10/03'),
+(12333465,'Eduardo','Gutierrez','eg202@gmail.com','M','1972/03/31','2019/07/15'),
+(17458946,'Carmen','Diaz','cd314@gmail.com','F','1985/05/24','2024/08/03'),
 
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (14578991,'Maria','Martin','maria.martin@hotmail.com','F','1976/11/30','2019/02/02');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (13524165,'Alondra','Diaz','ad@hotmail.com','F','1974/04/28','2020/09/03');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (21475896,'Andres','Lopez','al310@hotmail.com','M','1992/06/08','2015/07/02');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (21256485,'Casandra','Torres','cass@hotmail.com','F','1992/11/04','2022/10/05');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (32102456,'Amber','Paez','ambp@hotmail.com','F','2005/07/23','2023/06/13');
+(3965481,'Luis','Fernandez','lf@hotmail.com','M','1950/04/18','2016/04/26'),
+(25221031,'Miguel','Fernandes','mf@hotmail.com','M','1995/08/12','2024/09/13'),
+(19876324,'Jose','Lopez','jl@hotmail.com','M','1980/04/22','2023/09/07'),
+(26546128,'Paul','Moreno','paul1.0@hotmail.com','M','1998/06/14','2025/10/01'),
+(30111714,'Miranda','Perez','mperez@hotmail.com','F','2003/05/04','2023/11/21'),
 
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (6561123,'Gabriel','Lopez','gbrlp@yahoo.es','M','1968/02/01','2022/11/05');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (8756412,'Jose','Gil','jg@yahoo.es','M','1983/04/03','2021/12/25');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (28896451,'Ricardo','Carmona','rc@yahoo.es','M','2000/01/01','2020/01/04');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (14754123,'Mateo','Serrano','serrano@yahoo.es','M','1980/06/01','2022/04/03');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (1024585,'Carol','Molina','molina13@yahoo.es','F','1970/06/14','2021/09/03');
+(14578991,'Maria','Martin','maria.martin@hotmail.com','F','1976/11/30','2019/02/02'),
+(13524165,'Alondra','Diaz','ad@hotmail.com','F','1974/04/28','2020/09/03'),
+(21475896,'Andres','Lopez','al310@hotmail.com','M','1992/06/08','2015/07/02'),
+(21256485,'Casandra','Torres','cass@hotmail.com','F','1992/11/04','2022/10/05'),
+(32102456,'Amber','Paez','ambp@hotmail.com','F','2005/07/23','2023/06/13'),
 
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (13264458,'Alvaro','Carmona','alv@yahoo.es','M','1975/02/27','2022/09/06');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (14442135,'Sofia','Medida','sm@yahoo.es','F','1976/02/12','2024/08/03');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (26854456,'Marcela','Ortiz','m.ortiz@yahoo.es','F','1996/04/03','2022/11/18');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (7546851,'Isabel','Morales','isam@yahoo.es','F','1969/04/19','2019/11/09');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (12324530,'Braulio','Carmona','brauc@yahoo.es','M','2002/05/03','2024/08/03');
+(6561123,'Gabriel','Lopez','gbrlp@yahoo.es','M','1968/02/01','2022/11/05'),
+(8756412,'Jose','Gil','jg@yahoo.es','M','1983/04/03','2021/12/25'),
+(28896451,'Ricardo','Carmona','rc@yahoo.es','M','2000/01/01','2020/01/04'),
+(14754123,'Mateo','Serrano','serrano@yahoo.es','M','1980/06/01','2022/04/03'),
+(1024585,'Carol','Molina','molina13@yahoo.es','F','1970/06/14','2021/09/03'),
 
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (24157898,'Oscar','Mendez','oscarm@gmail.com','M','2000/03/18','2024/08/03');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (10213541,'Ismael','Castillo','ic@hotmail.com','M','1970/04/13','2022/06/21');
-INSERT INTO cliente(CI, nombre, apellido, correo, sexo, fechaNacimiento, fechaRegistro)
-VALUES (26785569,'Andres','Guerrero','ag@yahoo.es','M','1996/09/21','2018/05/04');
-
+(13264458,'Alvaro','Carmona','alv@yahoo.es','M','1975/02/27','2022/09/06'),
+(14442135,'Sofia','Medida','sm@yahoo.es','F','1976/02/12','2024/08/03'),
+(26854456,'Marcela','Ortiz','m.ortiz@yahoo.es','F','1996/04/03','2022/11/18'),
+(7546851,'Isabel','Morales','isam@yahoo.es','F','1969/04/19','2019/11/09'),
+(12324530,'Braulio','Carmona','brauc@yahoo.es','M','2002/05/03','2024/08/03'),
 
 --SELECT * From Cliente;	
