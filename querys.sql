3b63944c982b249bc9e3e6e2a52b0a0e78318a4b
maryxp <117952348+maryxp@users.noreply.github.com>
2025-02-25 19:56:26 -0400

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -15,7 +15,7 @@ trabajados
 SELECT T1.*
 FROM(SELECT E.CI, E.nombre, E.apellido, E.sexo, C.nombre, C.salarioBasePorHora, E.bonoFijoMensual, 
       (E.bonoFijoMensual + C.salarioBasePorHora) as totalObtenidoPorMes, 
-      (DATEDIFF(month,fechaContrato, curDate())/30.4)* T1.totalObtenidoPorMes as montoTotalDesdeSuInicio    --**
+      (DATEDIFF(month, E.fechaContrato, curDate())/30.4)* T1.totalObtenidoPorMes as montoTotalDesdeSuInicio    --**
     FROM Empleado E, Cargo C 
     ) as T1
 
